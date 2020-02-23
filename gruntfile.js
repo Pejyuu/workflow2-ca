186db16a2a09181cdf1071ec7c2ed8e61e52aa91
Pejyuu <marianne@mholmen.no>
2020-02-23 21:52:32 +0100

    Stuff and Things

--- gruntfile.js
+++ gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
           tasks: ['cssmin']
       },
       imageopti: {
-          files: ['app/images/*.*'],
+          files: ['app/images/*'],
           tasks: ['imagemin']
      }
     }
